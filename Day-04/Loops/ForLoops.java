9e740d1a4fa9fcf353febc1b432fa7b3f871bcfd
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-08-23 18:04:26 +0530

    Day 04: Practice loops in Java

--- Day-04/Loops/ForLoops.java
+++ Day-04/Loops/ForLoops.java
@@ -3,7 +3,7 @@ public class ForLoops{
     public static void main(String[] args) {
 
         // 1. Print numbers from 1 to 10
-        System.out.println("Numbers from 1 to 10:");
+        System.out.println("Numbers from 1 to 10:"); 
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
