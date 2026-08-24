0823f0e0c3838130b2fcf7b3db8296faee933b4e
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-08-24 19:44:20 +0530

    Day 05: Practice methods in Java

---
+++ Day-05/Methods.java
@@ -0,0 +1,24 @@
+public class Methods {
+
+    // Method without parameters
+    static void table() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(2 * i);
+        }
+    }
+
+    // Method with parameters
+    static void sum(int x, int y) {
+        int sum = x + y;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Table of 2:");
+        table();
+
+        System.out.println("Sum:");
+        sum(8, 9);
+    }
+}
