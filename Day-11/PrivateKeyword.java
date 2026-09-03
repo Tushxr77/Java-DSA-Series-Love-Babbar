95b7fd50d81af73d644b6d7bdae43fee0a9b5d5f
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-09-03 10:13:12 +0530

    Day 10: Practice Privatekeywords in Java

---
+++ Day-11/PrivateKeyword.java
@@ -0,0 +1,21 @@
+package OOPS;
+class Student {
+    String name;
+    private int id;
+    double cgpa;
+    public void print(){
+        System.out.println(name+" "+id+" "+cgpa);
+    }
+}
+public class PrivateKeyword {
+
+
+    public static void main (String[] args){
+        Student s1 = new Student();
+        s1.print();
+        s1.cgpa =7.2;
+        s1.name = "Tushar";
+       //s1.id = 21;
+        s1.print();
+    }
+}
