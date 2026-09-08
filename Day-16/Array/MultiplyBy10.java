c970c11c0f11232b8441c5dcd6173bd41c3db6dc
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-09-08 19:29:53 +0530

    Day 14: Practice Multiply Each Element Of Array By 10 In java

---
+++ Day-16/Array/MultiplyBy10.java
@@ -0,0 +1,27 @@
+public class MultiplyElementBy10 {
+
+    static int[] multiplyBy10(int[] arr) {
+        int size = arr.length;
+        int[] brr = new int[size]; // creating same size array
+
+        for (int i = 0; i < size; i++) {
+            int element = arr[i];
+            int newElement = element * 10;
+            brr[i] = newElement;
+        }
+
+        return brr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 9, 5};
+
+        int[] ans = multiplyBy10(arr);
+
+        System.out.println("Printing ans array");
+
+        for (int i : ans) {
+            System.out.print(" " + i);
+        }
+    }
+}
