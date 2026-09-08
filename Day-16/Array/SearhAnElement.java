662fbcc2d4d60a0cb4d964c88725bcaa15e959cc
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-09-08 19:33:09 +0530

    Day 14: Practice Search for an element in an array in Java

---
+++ Day-16/Array/SearhAnElement.java
@@ -0,0 +1,21 @@
+public class SearchAnElement {
+
+    static boolean search(int[] arr, int target) {
+        int size = arr.length;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 6, 9, 3};
+        int n = 3;
+
+        System.out.println(search(arr, n));
+    }
+}
