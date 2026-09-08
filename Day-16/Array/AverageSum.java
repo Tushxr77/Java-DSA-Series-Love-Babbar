52308dade434a748ff71e9340e868ded22953282
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-09-08 19:26:33 +0530

    Day 16: Practice Average  Of Element in Java

---
+++ Day-16/Array/AverageSum.java
@@ -0,0 +1,20 @@
+public class AverageSum {
+
+    static double getAverage(int[] arr) {
+        double sum = 0;
+
+        for (int i : arr) {
+            sum += i;
+        }
+
+        int size = arr.length;
+        double avg = sum / size;
+
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3};
+        System.out.println(getAverage(arr));
+    }
+}
